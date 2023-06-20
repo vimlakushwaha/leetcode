497e29226d42131571abb3c51dc14d1c94da13ee
vimlakushwaha <136914851+vimlakushwaha@users.noreply.github.com>
2023-06-20 14:57:36 +0530

    Time: 21 ms (94.58%), Space: 14.2 MB (87.95%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2){
+    if(root1 ==NULL) return root2;
+    if (root2 == NULL) return root1;
+    struct TreeNode* root;
+    root = root1;
+    root->val += root2->val;
+    root1->left = mergeTrees(root1->left,root2->left);
+    root1->right=mergeTrees(root1->right,root2->right);
+    return root;
+    
+   
+
+}
